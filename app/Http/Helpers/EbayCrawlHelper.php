68d5dd36273467109fb3a5de623d30af3d73554b
Huy Tran H. VN.Danang <108646730+mvn-huytran-dn@users.noreply.github.com>
2022-12-09 23:18:28 +0700

    fix code

--- app/Http/Helpers/EbayCrawlHelper.php
+++ app/Http/Helpers/EbayCrawlHelper.php
@@ -28,9 +28,26 @@ class EbayCrawlHelper
      */
     public static function httpRequest($crawlUrls)
     {
+        $headers = [
+            'authority'                 => 'suchen.mobile.de',
+            'accept'                    => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'accept-language'           => 'en-US,en;q=0.9,vi;q=0.8',
+            'cache-control'             => 'max-age=0',
+            'sec-ch-ua'                 => '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
+            'sec-ch-ua-mobile'          => '?0',
+            'sec-ch-ua-platform'        => '"macOS"',
+            'sec-fetch-dest'            => 'document',
+            'sec-fetch-mode'            => 'navigate',
+            'sec-fetch-site'            => 'cross-site',
+            'sec-fetch-user'            => '?1',
+            'upgrade-insecure-requests' => '1',
+            'user-agent'                => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
+            'Connection'                => 'keep-alive'
+        ];
+
         $clientSetting = ['allow_redirects' => ['track_redirects' => true], 'verify' => false, 'timeout'  => 60];
         $client = new Client($clientSetting);
-        $request = new Request('GET', $crawlUrls);
+        $request = new Request('GET', $crawlUrls, $headers);
         $res = $client->sendAsync($request)->wait();
         return $res->getBody()->getContents();
     }
